092ca1ab97cf25cf4b0f931f9ae3ad224f6f8f30
Prakhar Gandhi <gprakhar0@gmail.com>
2026-08-22 13:50:57 +0530

    Added recommender systems...

---
+++ movieRecommender.py
@@ -0,0 +1,81 @@
+# uvicorn movieRecommender:app --reload
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+import json
+from fastapi import FastAPI
+import mpire
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+import time
+import multiprocessing 
+from mpire import WorkerPool
+from pprint import pprint
+from typing import List
+from fastapi import FastAPI, Query
+from typing import List
+
+import asyncio
+from fastapi import FastAPI, Query
+from typing import List
+
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key not found!")
+client=Groq(api_key=my_api_key)
+
+def get_choice(response):
+    return response.choices[0].message.content
+
+def get_response(model,messages,response_format):
+    if response_format:
+        return client.chat.completions.create(model=model, messages=messages, response_format=response_format)
+    return client.chat.completions.create(model=model, messages=messages)
+    
+def run_videoLinksSuggestion_execution(movie_genre:str,model:str)->str:
+    role1 = "system"
+    role2 = "user"
+    ROLE = "role"
+    CONTENT = "content"
+    messages = [
+        {ROLE:role1,CONTENT:f"Suggest some of the best movies according to this genre type. Please do not hallucinate."},
+        {ROLE:role2,CONTENT:movie_genre},
+    ]
+    
+    topic_response = get_response(model,messages,None)
+    topic = get_choice(topic_response)
+    return {"request":movie_genre,"response":topic}
+
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI(
+    title="My API",
+    version="1.0.0"
+)
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
+@app.get("/movieRecommender/")
+def read_item(model:str,movie_genre:str):
+    results = run_videoLinksSuggestion_execution(movie_genre,model)
+    return results
+
+
+
+
+
+
+
+
+
+
+
---
+++ movieRecommenders.py
@@ -0,0 +1,86 @@
+# uvicorn movieRecommenders:app --reload
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+import json
+from fastapi import FastAPI
+import mpire
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+import time
+import multiprocessing 
+from mpire import WorkerPool
+from pprint import pprint
+from typing import List
+from fastapi import FastAPI, Query
+from typing import List
+
+import asyncio
+from fastapi import FastAPI, Query
+from typing import List
+
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key not found!")
+client=Groq(api_key=my_api_key)
+
+def get_choice(response):
+    return response.choices[0].message.content
+
+def get_response(model,messages,response_format):
+    if response_format:
+        return client.chat.completions.create(model=model, messages=messages, response_format=response_format)
+    return client.chat.completions.create(model=model, messages=messages)
+    
+def run_movieSuggestion_execution(movie_genre:str,model:str)->str:
+    role1 = "system"
+    role2 = "user"
+    ROLE = "role"
+    CONTENT = "content"
+    messages = [
+        {ROLE:role1,CONTENT:f"Suggest some of the best movies according to this genre type. Please do not hallucinate."},
+        {ROLE:role2,CONTENT:movie_genre},
+    ]
+    
+    topic_response = get_response(model,messages,None)
+    topic = get_choice(topic_response)
+    return {"request":movie_genre,"response":topic}
+
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI(
+    title="My API",
+    version="1.0.0"
+)
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
+@app.get("/movieRecommenders/")
+def read_item(model:str,progress_bar:bool,movie_genres:List[str] = Query(...)):
+    num_cores = max(min(multiprocessing.cpu_count() // 2, 2),1)
+    results = []
+    for movie_genre in movie_genres:
+        my_dict = {
+            "movie_genre":movie_genre,
+            "model":model
+        }
+        results.append(my_dict)
+    with WorkerPool(n_jobs=num_cores,daemon=False) as pool:
+        results = pool.map(run_movieSuggestion_execution, results, progress_bar=progress_bar)
+    return results
+
+
+
+
+
+
+
