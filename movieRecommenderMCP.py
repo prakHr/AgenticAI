b737a184bb0b06a67f1d51147ba1fdebc649d174
Prakhar Gandhi <gprakhar0@gmail.com>
2026-08-29 09:36:36 +0530

    Added MCP

---
+++ movieRecommenderMCP.py
@@ -0,0 +1,103 @@
+# uvicorn movieRecommender:app --reload
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+import json
+from fastapi import FastAPI
+import mpire
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+import time
+import multiprocessing 
+from mpire import WorkerPool
+from pprint import pprint
+from typing import List
+from fastapi import FastAPI, Query
+from typing import List
+
+import asyncio
+from fastapi import FastAPI, Query
+from typing import List
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi_mcp import FastApiMCP
+
+
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key not found!")
+client=Groq(api_key=my_api_key)
+
+def get_choice(response):
+    return response.choices[0].message.content
+
+def get_response(model,messages,response_format):
+    if response_format:
+        return client.chat.completions.create(model=model, messages=messages, response_format=response_format)
+    return client.chat.completions.create(model=model, messages=messages)
+    
+def run_movieSuggestion_execution(movie_genre:str,model:str)->str:
+    role1 = "system"
+    role2 = "user"
+    ROLE = "role"
+    CONTENT = "content"
+    messages = [
+        {ROLE:role1,CONTENT:f"Suggest some of the best movies according to this genre type. Please do not hallucinate."},
+        {ROLE:role2,CONTENT:movie_genre},
+    ]
+    
+    topic_response = get_response(model,messages,None)
+    topic = get_choice(topic_response)
+    return {"request":movie_genre,"response":topic}
+
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+filename = os.path.basename(__file__).split(".")[0]
+app = FastAPI(
+    title=f"{filename}",
+    version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
+@app.get("/movieRecommender/",operation_id="recommend_movie")
+def read_item(model:str,movie_genre:str):
+    results = run_movieSuggestion_execution(movie_genre,model)
+    return results
+
+
+if __name__=="__main__":
+    mcp = FastApiMCP(app,include_operations = ["recommend_movie"])
+    mcp.mount_http()
+    import uvicorn
+    uvicorn.run(app,host="0.0.0.0",port=8000)
+
+
+
+
+
+
+
+
+
+
+
